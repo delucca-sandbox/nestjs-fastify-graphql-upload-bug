57a6e844cc832943394dbfa90f0b55bde4b06d5b
Daniel De Lucca <deluccafonseca@gmail.com>
2021-04-15 20:21:19 +0000

    wip

--- src/user.input-object.ts
+++ src/user.input-object.ts
@@ -1,9 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { Exclude } from 'class-transformer';
 import { FileUpload } from 'graphql-upload';
 import { UploadGraphQLScalar } from './upload.scalar';
 
 @InputType()
 export class UserInputObject {
   @Field(() => UploadGraphQLScalar)
+  @Exclude()
   public readonly picture?: Promise<FileUpload>;
 }
