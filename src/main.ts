182ac1223e8ad2af89a947fa660dbaed893e420c
Daniel De Lucca <deluccafonseca@gmail.com>
2021-04-15 19:26:31 +0000

    find

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { processRequest } from 'graphql-upload';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const adapter = new FastifyAdapter();
@@ -21,6 +22,11 @@ async function bootstrap() {
   });
 
   const app = await NestFactory.create(AppModule, adapter);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
